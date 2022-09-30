d55210fe4f4e48db8cf53c760829d6faeb06bdba
Jim00000 <9379786+Jim00000@users.noreply.github.com>
2022-09-30 23:39:57 +0800

    update version tag to 0.9.2

--- zr2cheat/Jim00000_ZR2_Cheat.js
+++ zr2cheat/Jim00000_ZR2_Cheat.js
@@ -24,8 +24,8 @@
 // Jim00000's cheat script for Zombie's Retreat 2
 // --------------------------------------------------------------------------------
 // ▶ Author         : Jim00000
-// ▶ Target process : Zombie's Retreat 2 - Beta 0.8.1
-// ▶ Update         : 07.09.2022
+// ▶ Target process : Zombie's Retreat 2 - Beta 0.9.2
+// ▶ Update         : 10.01.2022
 // ▶ License        : GNU GENERAL PUBLIC LICENSE Version 3
 // --------------------------------------------------------------------------------
 
@@ -40,7 +40,7 @@
     let is_full_item_enabled = false;
     let enemy_count = 0;
     let original_color_tone = [];
-    const supported_game_version = 'beta 0.8.1'
+    const supported_game_version = 'beta 0.9.2'
     const toggle_cheat_panel_virtualkey = 118  // F7
     const remove_all_enemies_virtualkey = 119  // F8
     const remove_all_enemies_keyname = 'remove_all_enemies';
