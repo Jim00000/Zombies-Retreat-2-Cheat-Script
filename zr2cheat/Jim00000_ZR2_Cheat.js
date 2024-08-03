9aef3f4f620a623721659abf1ddec1ab485bb44c
Jim00000 <9379786+Jim00000@users.noreply.github.com>
2024-08-03 17:16:31 +0800

    Bump version to v0.19.2
    Signed-off-by: Jim00000 <9379786+Jim00000@users.noreply.github.com>

--- zr2cheat/Jim00000_ZR2_Cheat.js
+++ zr2cheat/Jim00000_ZR2_Cheat.js
@@ -20,8 +20,8 @@
 // Jim00000's cheat script for Zombie's Retreat 2
 // --------------------------------------------------------------------------------
 // ▶ Author         : Jim00000
-// ▶ Target process : Zombie's Retreat 2 - Beta 0.16.2
-// ▶ Update         : 12.09.2023
+// ▶ Target process : Zombie's Retreat 2 - Beta 0.19.2
+// ▶ Update         : 08.03.2024
 // ▶ License        : GNU GENERAL PUBLIC LICENSE Version 3
 // --------------------------------------------------------------------------------
 
@@ -31,7 +31,7 @@ var speed_multiplier = 1.0;
 var fadeEffectHandlerId = -1;
 var is_zombie_freezed = false;
 var enemy_count = 0;
-var supported_game_version = 'beta 0.16.2';
+var supported_game_version = 'beta 0.19.2';
 var original_zr2_title = document.title;
 var enemy_name_list = [
     'HC_Zombies2A', 'HC_Zombies2B', 'HC_Zombies2C', 'HC_Zombies2D',
